d24b4cfe0fd538c992419e4dcf986799166ccd40
liyazhu <15210134184@163.com>
2020-03-04 19:33:17 +0800

    新增测试

---
+++ Test222/Test222/ZhuAddTest.h
@@ -0,0 +1,17 @@
+//
+//  ZhuAddTest.h
+//  Test222
+//
+//  Created by 珠珠 on 2020/3/4.
+//  Copyright © 2020 zhuzhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZhuAddTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test222/Test222/ZhuAddTest.m
@@ -0,0 +1,13 @@
+//
+//  ZhuAddTest.m
+//  Test222
+//
+//  Created by 珠珠 on 2020/3/4.
+//  Copyright © 2020 zhuzhu. All rights reserved.
+//
+
+#import "ZhuAddTest.h"
+
+@implementation ZhuAddTest
+
+@end
