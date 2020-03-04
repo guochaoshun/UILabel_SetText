55a38c6014929855f5acf60b688a51999c0f3920
liyazhu <15210134184@163.com>
2020-03-04 20:11:41 +0800

    添加main2

---
+++ Test222/Test222/Main2.h
@@ -0,0 +1,17 @@
+//
+//  Main2.h
+//  Test222
+//
+//  Created by 珠珠 on 2020/3/4.
+//  Copyright © 2020 zhuzhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Main2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test222/Test222/Main2.m
@@ -0,0 +1,13 @@
+//
+//  Main2.m
+//  Test222
+//
+//  Created by 珠珠 on 2020/3/4.
+//  Copyright © 2020 zhuzhu. All rights reserved.
+//
+
+#import "Main2.h"
+
+@implementation Main2
+
+@end
